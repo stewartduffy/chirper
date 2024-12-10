492e794d36ca049c7bef2d236de57c71914afa6e
Stewart Duffy <duffy.stewart@gmail.com>
2024-12-10 21:49:17 +1300

    Create and show chirps

---
+++ lang/en/messages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Application Messages
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used across your application for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Dashboard' => 'Dashboard',
+    'Chirps' => 'Hello Chirps',
+    // Add other translation strings as needed
+];
